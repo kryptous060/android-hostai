41ff474d0886c85f8bb3a8ee2ecfd58320b92382
kryptous060 <kryptous606@gmail.com>
2026-06-18 14:56:48 -0500

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ buildscript {
     dependencies {
         // Upgraded to 8.7.0 to align with Gradle 8.7 and support Kotlin 2.2.0 metadata
         classpath("com.android.tools.build:gradle:8.7.0")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.2.0")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.0.21")
     }
 }
 
