66d083dbe657d7324308388f5a2799dd213db4cb
kryptous060 <kryptous606@gmail.com>
2026-06-18 13:28:39 -0500

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,8 @@ buildscript {
         mavenCentral()
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:8.5.2")
+        // Updated from 8.5.2 to 8.6.0 to fix the D8 compiler metadata error
+        classpath("com.android.tools.build:gradle:8.6.0")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.2.0")
     }
 }
